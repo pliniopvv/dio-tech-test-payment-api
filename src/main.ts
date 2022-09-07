171f671036b05bc7150c07b8b47e0b4bfb4f2c0b
pliniopvv <gangss2@Hotmail.com>
2022-09-07 16:03:00 -0300

    swagger funcionando na rota /api

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-  .setTitle('Cats example')
-  .setDescription('The cats API description')
-  .setVersion('1.0')
-  .addTag('cats')
+  // .setTitle('Cats example')
+  // .setDescription('The cats API description')
+  // .setVersion('1.0')
+  // .addTag('cats')
   .build();
   const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
