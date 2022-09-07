8f1db26b80aa6267cbf2ee630b2106c9eeb01e43
pliniopvv <gangss2@Hotmail.com>
2022-09-07 16:20:25 -0300

    +dotenv e typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { env } from './utils/Utils.tools';
+
+let DB_SOURCE = env('DB_SOURCE');
+let _entities = [];
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: DB_SOURCE,
+      entities: _entities,
+      synchronize: true,
+  }),
+],
   controllers: [AppController],
   providers: [AppService],
 })
